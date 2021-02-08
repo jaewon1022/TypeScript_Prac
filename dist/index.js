b7ce2702c57142a0c7a27fb4d024484ad7482ac2
JW <andyjaewon@naver.com>
2021-02-08 15:38:13 +0900

    Creating Block part 2 Done.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timeStamp) {
         this.index = index;
@@ -9,15 +10,10 @@ class Block {
         this.timeStamp = timeStamp;
     }
 }
-function randomHashMaker() {
-    const alphabetChar = 'abcdefghijklmnopqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const alphabetCharLen = alphabetChar.length;
-    let hash = "";
-    for (let i = 0; i < 20; i++) {
-        hash += alphabetChar.charAt(Math.floor(Math.random() * alphabetCharLen));
-    }
-    return hash;
-}
-const genesisBlock = new Block(0, randomHashMaker(), "", "Hello", 123456);
-console.log(genesisBlock);
+Block.calculateBlockHash = (index, previousHash, timeStamp, data) => CryptoJS.SHA256(index + previousHash + timeStamp + data).toString();
+const genesisBlock = new Block(0, "202020202020", "", "Hello", 123456);
+let blockChain = [genesisBlock];
+const getBlockChain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 //# sourceMappingURL=index.js.map
